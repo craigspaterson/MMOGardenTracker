09a1c3b04ccc0e412ac8a18d911ee73d0ebdc2f2
craigspaterson <craigspaterson@outlook.com>
2020-12-26 12:39:20 -0800

    WIP-SRC-10 Completed

--- WebService/GT.Web.Api/Models/ErrorDetails.cs
+++ WebService/GT.Web.Api/Models/ErrorDetails.cs
@@ -1,15 +1,19 @@
-﻿using System.Text.Json;
-
-namespace GT.Web.Api.Models
+﻿namespace GT.Web.Api.Models
 {
+    /// <summary>
+    /// ErrorDetails
+    /// </summary>
     public class ErrorDetails
     {
+        /// <summary>
+        /// Gets or sets the status code.
+        /// </summary>
+        /// <value>The status code.</value>
         public int StatusCode { get; set; }
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <value>The message.</value>
         public string Message { get; set; }
-
-        public override string ToString()
-        {
-            return JsonSerializer.Serialize(this);
-        }
     }
 }
