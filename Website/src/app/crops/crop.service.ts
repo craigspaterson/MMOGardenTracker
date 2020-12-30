c035d8e66936a566c397bb49ac98b2237e2cbe52
craigspaterson <craigspaterson@outlook.com>
2020-12-30 10:44:29 -0800

    WIP-SRC-11 npm packages

--- Website/src/app/crops/crop.service.ts
+++ Website/src/app/crops/crop.service.ts
@@ -23,7 +23,7 @@ export class CropService {
   // private url = 'api/crops';
   private url: string = environment.gardenTrackerServiceUrl + 'api/crops';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getCrops(): Observable<Crop[]> {
     return this.http
@@ -58,7 +58,7 @@ export class CropService {
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
+      console.error(`An error occurred: ${error.error.message}`);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
@@ -66,6 +66,11 @@ export class CropService {
         `Backend returned code ${error.status}, ` + `body was: ${error.error}`
       );
     }
+
+    console.log(`The error status: ${error.status}`);
+    console.log(`The error message: ${error.message}`);
+    console.table(error);
+
     // return an observable with a user-facing error message
     return throwError('Something bad happened; please try again later.');
   }
