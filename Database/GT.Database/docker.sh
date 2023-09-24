5fdea38f83f160e448caca75bf9b1822d76c46f3
craigspaterson <craigspaterson@outlook.com>
2023-09-23 18:12:49 -0700

    update script

--- Database/GT.Database/docker.sh
+++ Database/GT.Database/docker.sh
@@ -8,8 +8,11 @@
 # Pull the latest SQL Server 2019 container image from Microsoft Container Registry
 # sudo docker pull mcr.microsoft.com/mssql/server:2022-latest
 
+# Pull and Run the SQL Server container
+# sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=YourStrong@Passw0rd" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
+
 # Run the SQL Server container
-sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=YourStrong@Passw0rd" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
+sudo docker start sql1
 
 # Connect to the SQL Server container
 # sudo docker exec -it sql1 "bash"
